dd8a0410464fb47da19d96aebcb2a709912e9a1e
LuisLiraC <luis_ssbm@hotmail.com>
2020-03-19 00:16:37 -0600

    Withoout compression

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import compression from 'compression'
+// import compression from 'compression'
 import cors from 'cors'
 import { ApolloServer } from 'apollo-server-express'
 import { schema } from './schema'
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000
 
 const app = express()
 app.use(cors())
-app.use(compression())
+// app.use(compression())
 app.use(express.json())
 
 const server = new ApolloServer({
